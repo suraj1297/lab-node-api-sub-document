8cd98dcb7fe0e17a5286222af2194d1ca06d1e8f
Suraj Desai <22217748+suraj1297@users.noreply.github.com>
2020-11-20 16:33:15 +0530

    Update connection.js

--- connection/connection.js
+++ connection/connection.js
@@ -2,7 +2,7 @@ const { model } = require("mongoose")
 const mongoose = require("mongoose")
 
 
-url = 'mongodb+srv://admin:root@testing.l4cpi.mongodb.net/Clasroom?retryWrites=true&w=majority'
+url = process.env.URL
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
     .then(response => console.log("conected to database successfully"))
